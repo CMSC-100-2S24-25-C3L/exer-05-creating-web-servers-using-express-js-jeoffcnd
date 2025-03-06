9ba9c499a36eccba4a4be213b2682516dc74b760
Jeoff Nathaniel M. Conde <jeoffnathanielconde@Jeoffs-MacBook-Air.local>
2025-03-06 20:31:28 +0800

    chore: added some tests

--- test.js
+++ test.js
@@ -1,3 +1,31 @@
 import needle from 'needle';
 
 const BASE_URL = 'http://localhost:3000';
+
+
+// Run tests
+(async () => {
+    const book1 = {
+        book_name: "Harry Potter and the Philosopher’s Stone",
+        ISBN: "978-0-7475-3269-9",
+        author: "J.K. Rowling",
+        year_published: "1997"
+    };
+
+    const book2 = {
+        book_name: "The Hobbit",
+        ISBN: "978-0-261-10221-7",
+        author: "J.R.R. Tolkien",
+        year_published: "1937"
+    };
+
+    console.log("Testing Add Book:");
+    await addBook(book1);
+    await addBook(book2);
+
+    console.log("\nTesting Find by ISBN and Author:");
+    await findByIsbnAndAuthor("978-0-7475-3269-9", "J.K. Rowling");
+
+    console.log("\nTesting Find by Author:");
+    await findByAuthor("J.K. Rowling");
+})();
